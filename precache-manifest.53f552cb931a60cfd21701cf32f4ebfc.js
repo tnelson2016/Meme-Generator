2d7710054ceff9a4e057de22fc5a11f4ac75f135
Your Name <you@example.com>
2019-07-15 20:07:49 -0400

    Updates

---
+++ precache-manifest.53f552cb931a60cfd21701cf32f4ebfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f82bf80e80855f2a4a19a2627a873b",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "5118a7254a645237b194",
+    "url": "/Meme-Generator/static/css/main.e8010df9.chunk.css"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/Meme-Generator/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "5118a7254a645237b194",
+    "url": "/Meme-Generator/static/js/main.96639abe.chunk.js"
+  },
+  {
+    "revision": "a1b3edea92dd6eed2d90",
+    "url": "/Meme-Generator/static/js/runtime~main.730703d4.js"
+  }
+]);
